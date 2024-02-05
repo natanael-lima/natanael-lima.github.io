465e2ddd59827e92b5c9d89d62979f7459d3f18b
lima73777@gmail.com <lima73777@gmail.com>
2024-02-04 21:06:08 -0300

    mejor version

--- js/script.js
+++ js/script.js
@@ -1,71 +1,17 @@
-// Muestra el spinner
-var spinnerContainer = document.querySelector('.spinner-container');
-spinnerContainer.style.display = 'flex';
-
-// Oculta el spinner después de 3 segundos
-setTimeout(function () {
-  spinnerContainer.style.display = 'none';
-}, 3000); // 3000 milisegundos = 3 segundos
-
-
-//Menu para cambiar de color al hacer clic hover
-/*document.addEventListener("DOMContentLoaded", function () {
-	const sections = document.querySelectorAll("section");
-	const links = document.querySelectorAll("nav a");
+document.addEventListener("DOMContentLoaded", function() {
+	const aboutBtn = document.getElementById("aboutBtn");
+	const skillBtn = document.getElementById("skillBtn");
+	const aboutContent = document.getElementById("aboutContent");
+	const skillContent = document.getElementById("skillContent");
   
-	window.addEventListener("scroll", () => {
-	  sections.forEach((section) => {
-		const top = window.scrollY;
-		const offset = section.offsetTop;
-		const height = section.offsetHeight;
-		const id = section.getAttribute("id");
-		
-		if (top >= offset && top < offset + height) {
-		  links.forEach((link) => {
-			link.classList.remove("active-link");
-		  });
-		  document.querySelector('nav a[href*="' + id + '"]').classList.add("active-link");
-		}
-	  });
+	aboutBtn.addEventListener("click", function() {
+	  aboutContent.classList.add("active");
+	  skillContent.classList.remove("active");
 	});
-  });*/
-  document.addEventListener("DOMContentLoaded", function () {
-	const links = document.querySelectorAll("nav a");
-
-	links.forEach((link) => {
-	  link.addEventListener("click", function () {
-		// Remover la clase 'active-link' de todos los enlaces
-		links.forEach((otherLink) => {
-		  otherLink.classList.remove("active-link");
-		});
-
-		// Agregar la clase 'active-link' al enlace clicado
-		link.classList.add("active-link");
-	  });
+  
+	skillBtn.addEventListener("click", function() {
+	  skillContent.classList.add("active");
+	  aboutContent.classList.remove("active");
 	});
   });
-
-
-// Responsive menu
-$(document).ready(function() {
-	$("#menu-icon").click(function() {
-	  $("#nav-list").toggleClass("active");
-	});
-  });
-
-// Función para abrir el menú móvil
-function openMobileMenu() {
-	document.getElementById("mobile-menu").classList.add("active");
-  }
   
-// Función para cerrar el menú móvil
-function closeMobileMenu() {
-	document.getElementById("mobile-menu").classList.remove("active");
-  }
-  
-// Eventos para abrir y cerrar el menú móvil
-  document.getElementById("menu-icon").addEventListener("click", openMobileMenu);
-  document.getElementById("mobile-menu").addEventListener("click", closeMobileMenu);
-
-
-//Backgroud
